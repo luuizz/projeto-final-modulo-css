c5b2f048a2d79a9fcce9631799428a12bf73171a
luuizz <luu.izz@hotmail.com>
2023-04-02 16:02:23 -0300

    faq finalizado ✌🙌

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-if (document.getElementById("faqclick"))
+class Accordion {
+    constructor(accordionListQuestions) {
+        this.accordionListQuestions = document.querySelectorAll(accordionListQuestions);
+        this.activeItemClass = "active";
+    }
+
+    toggleAccordion(item) {
+        item.classList.toggle(this.activeItemClass);
+        item.nextElementSibling.classList.toggle(this.activeItemClass);
+    }
+
+    addAccordionEvent() {
+        this.accordionListQuestions.forEach((question) => {
+            question.addEventListener("click", () => this.toggleAccordion (question));
+        });
+
+    }
+
+    init() {
+        if (document.getElementById("faqclick"))
     for (var btnContainer = document.getElementById("faqclick"), btns = btnContainer.getElementsByClassName("question"), i = 0, l = btns.length; i < l; i++)
         btns[i].onclick = function() {
             for (var e = 0; e < l; e++)
@@ -6,3 +25,11 @@ if (document.getElementById("faqclick"))
             this.classList.toggle("active")
         }
         ;
+
+        return this;
+
+    }
+}
+
+const accordion = new Accordion(".faqclick");
+accordion.init();
