fbd50fd83ec8e3bfbd04b0350ca97fc58f1ef6a9
luuizz <luu.izz@hotmail.com>
2023-04-01 23:04:45 -0300

    hero e segunda seção finalizados

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-import WaaarkLuge from "https://cdn.skypack.dev/@waaark/luge@0.6.1-beta";
+if (document.getElementById("faqclick"))
+    for (var btnContainer = document.getElementById("faqclick"), btns = btnContainer.getElementsByClassName("question"), i = 0, l = btns.length; i < l; i++)
+        btns[i].onclick = function() {
+            for (var e = 0; e < l; e++)
+                btns[e] != this && btns[e].classList.remove("active");
+            this.classList.toggle("active")
+        }
+        ;
